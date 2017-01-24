0ed1620eeca1d6eb221963591a8d4c5daaefaf7f
朱新辉 <zhuxinhui@imzhiliao.com>
2017-01-25 05:58:25 +0800

    update

---
+++ learn5.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+'''
+添加层
+'''
+
+import tensorflow as tf
+
+def add_layer(input,in_size,out_size,activation_function=None):
+	#一行代表一个神经元
+	Weights = tf.Variable(tf.random_normal([in_size,out_size]))
+	biases = tf.Variable(tf.zeros)
+	Wx_plus_b = tf.matmul(inputs,Weights) + biases
+	if activation_function is None:
+		outputs = Wx_plus_b
+	else:
+		outputs = activation_function(Wx_plus_b)
+	return outputs
