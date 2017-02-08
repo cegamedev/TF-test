9c40319cec2f389ad93eb9062df5e15baf5855d3
zhuxinhui <zhuxinhui@imzhiliao.com>
2017-02-08 08:01:37 +0800

    可视化1

--- learn4.py
+++ learn4.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 
 '''
-Variable的使用
+placeholder的使用
 '''
 import tensorflow as tf
 
 input1 = tf.placeholder(tf.float32)
 input2 = tf.placeholder(tf.float32)
 
-output = tf.mul(input1,input2)
+output = tf.mul(input1, input2)
 
 with tf.Session() as sess:
-	print(sess.run(output,feed_dict={input1:[7.0],input2:[2.]}))
+    print(sess.run(output, feed_dict={input1: [7.0], input2: [2.]}))
